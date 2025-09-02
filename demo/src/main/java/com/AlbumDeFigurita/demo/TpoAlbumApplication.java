989210f9882be6b21bd81f34223849b1b8aeab05
sofia lappano <sofiaselenelappano@gmail.com>
2025-09-02 14:18:42 -0300

    1

---
+++ demo/src/main/java/com/AlbumDeFigurita/demo/TpoAlbumApplication.java
@@ -0,0 +1,13 @@
+package com.AlbumDeFigurita.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpoAlbumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpoAlbumApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/AlbumDeFigurita/demo/TpoAlbumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AlbumDeFigurita.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpoAlbumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
